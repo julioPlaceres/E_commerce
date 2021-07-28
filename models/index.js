706e94389f0a21f828ad2e3627bc7b5b63fd1337
Julio Placeres <leondreamheart@outlook.com>
2021-07-28 19:12:23 -0400

    Added relation between tables

--- models/index.js
+++ models/index.js
@@ -6,13 +6,37 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category,{
-  
-})
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+});
+
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(ProductTag, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE"
+});
+
+ProductTag.hasOne(Product, {
+  foreignKey:"product_id",
+  onDelete: "CASCADE"
+});
+
+Tag.belongsTo(ProductTag, {
+  foreignKey: "tag_id",
+  onDelete: "CASCADE"
+});
+
+ProductTag.hasMany(Tag, {
+  foreignKey: "tag_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany
 
 module.exports = {
   Product,
